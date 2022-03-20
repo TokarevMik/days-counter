d46ad1673b2d5d88b5d0f3828252e2b5d5269838
TokarevMik <stiks84@gmail.com>
2022-03-20 18:48:41 +0500

    "ver 1.0 working logic "

--- src/Day.java
+++ src/Day.java
@@ -1,7 +1,25 @@
+import java.security.PrivateKey;
 import java.time.LocalDate;
 
 public class Day {
-    LocalDate date;
-    TypeOfDay typeOfDay;
+    private int numOfType;
 
+    public Day(int numOfType) {
+        this.numOfType = numOfType;
+    }
+
+    private String typeOfDay =
+            switch (numOfType) {
+                case 1 -> "Первая дневная смена";
+                case 2 -> "Вторая дневная смена";
+                case 3 -> "Первая ночная смена";
+                case 4 -> "Вторая ночная смена";
+                case 5 -> "Первый выходной";
+                case 6 -> "Второй выходной";
+                default -> "Неверное значение";
+            };
+
+    public String getTypeOfDay() {
+        return typeOfDay;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("dd.MM.yy");
-        String firstDate = "31.12.21";
-        String firstDay = String.valueOf(TypeOfDay.FIRSTOFFDAY);
-        String secDate = "15.01.22";
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите дату в формате dd.mm.yyyy");
+        String secDate = scn.nextLine();
+        scn.close();
+//        System.out.println(getDay("22.03.2022"));
+        System.out.println(getDay(secDate));
 
-//        Scanner scn = new Scanner(System.in);
-        LocalDate time1 = LocalDate.now(); // сегодняшняя дата
-        LocalDate time2  = LocalDate.parse(secDate,formatter);
-        System.out.println(time1.until(time2, ChronoUnit.DAYS));
+
+    }
+
+    public static String getDay(String secDate) {
+        String type = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate time1 = LocalDate.of(2022, 03, 16);//дата отсчета
+        LocalDate time2 = LocalDate.parse(secDate, formatter);
+        int typeOfDay = 1;
+        int days = (int) time1.until(time2, ChronoUnit.DAYS);
+        int nums;
+        if (days > 6) {
+            days %= 6;
+            nums = typeOfDay + days;
+        } else if (days < 6) {
+            nums = typeOfDay + days;
+        } else {
+            nums = 1;
+        }
+        type = switch (nums) {
+            case 1 -> "Первая дневная смена";
+            case 2 -> "Вторая дневная смена";
+            case 3 -> "Первая ночная смена";
+            case 4 -> "Вторая ночная смена";
+            case 5 -> "Первый выходной";
+            case 6 -> "Второй выходной";
+            default -> "Неверное значение";
+        };
+        return type;
     }
 
 
